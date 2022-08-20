4ad185abef2d0962890e1ab894bca53b5fa592ff
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-20 10:49:18 +0900

    [Feat] Order

---
+++ RC_week2/RC_week2/Sources/Order/OrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderViewController.swift
+//  RC_week2
+//
+//  Created by 소정의 Mac on 2022/08/20.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
