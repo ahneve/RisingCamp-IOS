7b216371d11364fac422f3ecb040ec92a6dab105
안소정 <80646857+ahneve@users.noreply.github.com>
2022-08-30 17:28:36 +0900

    RC_week4 프로젝트 생성

---
+++ RC_week4/RC_week4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RC_week4
+//
+//  Created by 소정의 Mac on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
