1ac99bafcd0a86cc6192f0a3a684c20e6b88c3b7
안소정 <80646857+ahneve@users.noreply.github.com>
2022-09-01 16:43:26 +0900

    DispatchQueue Test

--- RC_week4/RC_week4/ViewController.swift
+++ RC_week4/RC_week4/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        DispatchQueue.global().async {
+            for i in 0..<100{
+                print(i)
+            }
+        }
+        
+        DispatchQueue.global().async {
+            for i in 200..<300{
+                print(i)
+            }
+        }
+        
     }
 
+    //
 
 }
 
